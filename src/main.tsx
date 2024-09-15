4aa64c98fdd757dfa8c2e4cd3f9509eeedc97c71
shkor.danila@gmail.com <shkor.danila@gmail.com>
2024-09-15 15:51:27 +0300

    Project init

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { RouterProvider } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { router } from './routes/router'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+)
